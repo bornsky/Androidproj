c0eed012edfbc43ab4503e812deba0726ab26911
Courtney Osborne <c@nosborne.co>
2018-06-15 18:17:04 -0700

    code that works

--- app/src/main/java/com/example/court/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/court/projectandroid/MainActivity.java
@@ -1,21 +1,10 @@
 package com.example.court.projectandroid;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,78 +15,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final Button button = findViewById(R.id.button_id);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+                // Do something here with the button
             }
         });
-
-        Button btn = findViewById(R.id.pressBtn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        public static String converse (String host,int port, String path)throw IOException {
-
-            URL url = new URL("http", host, port, path);
-            URLConnection connection = null;
-            try {
-                connection = url.openConnection();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            //This does a GET
-            connection.setDoInput(true);
-            //To do a POST
-//            connection.setDoOutput(true);
-            connection.setAllowUserInteraction(true);
-
-            StringBuilder stringBuilder = new StringBuilder();
-
-            BufferedReader in = null;
-            try {
-                in = new BufferedReader(
-                        new InputStreamReader(connection.getInputStream()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            String line;
-
-            while ((line = in.readline()) != null) {
-                stringBuilder.append(line);
-            }
-
-            in.close();
-            return stringBuilder.toString();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
